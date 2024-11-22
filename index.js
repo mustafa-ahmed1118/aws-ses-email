53481c0862d56da22265ef4586f3515b463e0496
COMTECHTEL\mustafa.ahmed <mustafa.ahmed@comtech.com>
2024-11-22 12:57:01 -0500

    completed email service handler and fixed node version incompatabilities.

--- index.js
+++ index.js
@@ -1,13 +1,43 @@
-module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v3.0! Your function executed successfully!',
-        input: event,
+"use strict";
+const AWS = require("aws-sdk");
+const ses = new AWS.SES();
+
+module.exports.createContact = async (event, context) => {
+  console.log("Recived:::", event);
+  const {to, from , subject, message} = JSON.parse(event.body);
+
+  if (!to || !from || !subject || !message){
+    return{
+      statusCode: 400,
+      body: JSON.stringify({message: "to or from... are not set properly"})
+    };
+  }
+  const params = {
+    Destination: {
+      ToAddresses:[to]
+    },
+    Message: {
+      Body: {
+        Text: {Data : message},
       },
-      null,
-      2
-    ),
+      Subject: {Data : subject},
+    },
+    Source: from
   };
+  try{
+    await ses.sendEmail(params).promise();
+    return{
+      statusCode: 200,
+      body: JSON.stringify({message:"email sent succesfully!", 
+        success: true,
+      }),
+    };
+  }catch (error){
+    return{
+      statusCode: 400,
+      body: JSON.stringify({message:"email unsuccesful.", 
+        success: true,
+      }),
+    };
+  }
 };
